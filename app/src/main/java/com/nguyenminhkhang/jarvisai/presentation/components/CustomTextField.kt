2b4839235ec7a57eb6fcd269a9fd024a20bc36ad
Nguyễn Minh Khang <mkhang040506@gmail.com>
2025-11-16 00:54:31 +0700

    feat: Implement CustomTextField component and LoginScreen state management
    - Added CustomTextField composable with placeholder and title support.
    - Introduced LoginState data class to manage username, password, loading state, and error messages.
    - Created Color.kt and Theme.kt files to define color schemes for light and dark themes.
    - Established typography settings in Type.kt with Google Fonts integration.
    - Added font_certs.xml for Google Fonts certification.

--- app/src/main/java/com/nguyenminhkhang/jarvisai/presentation/components/CustomTextField.kt
+++ app/src/main/java/com/nguyenminhkhang/jarvisai/presentation/components/CustomTextField.kt
@@ -1,2 +1,49 @@
 package com.nguyenminhkhang.jarvisai.presentation.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomTextField(modifier: Modifier, text: String?, textFieldTitle: String) {
+    Column {
+        Text(
+            text = textFieldTitle,
+            modifier = Modifier.padding(bottom = 4.dp)
+        )
+        BasicTextField(
+            value = text ?: "",
+            onValueChange = {},
+            decorationBox = { innerTextField ->
+                Box(
+                    modifier = Modifier.fillMaxWidth().clip(shape = RoundedCornerShape(16.dp)).padding(12.dp)
+                ) {
+                    if(text.isNullOrEmpty()) {
+                        Text("Enter text here...", color = Color.LightGray)
+                    }
+                    innerTextField()
+                }
+            },
+            modifier = modifier.clip(shape = RoundedCornerShape(16.dp)).background(Color.Gray)
+        )
+    }
+
+}
+
+@Preview
+@Composable
+fun CustomTextFieldPreview() {
+
+    CustomTextField(modifier = Modifier.fillMaxWidth(), text ="", textFieldTitle = "Username")
+}
--- app/src/main/java/com/nguyenminhkhang/jarvisai/presentation/screens/LoginScreen.kt
+++ app/src/main/java/com/nguyenminhkhang/jarvisai/presentation/screens/LoginScreen.kt
@@ -1,2 +1,15 @@
 package com.nguyenminhkhang.jarvisai.presentation.screens
 
+import androidx.compose.runtime.Composable
+
+data class LoginState(
+    val username: String = "",
+    val password: String = "",
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null
+)
+
+@Composable
+fun LoginScreen() {
+
+}
