758dc22e9493af5be3e1e237b449f680aa5d7e34
Nguyễn Minh Khang <mkhang040506@gmail.com>
2025-11-16 10:37:55 +0700

    feat: Add LoginScreen component with UI elements for user authentication

--- app/src/main/java/com/nguyenminhkhang/jarvisai/presentation/screens/auth/LoginScreen.kt
+++ app/src/main/java/com/nguyenminhkhang/jarvisai/presentation/screens/auth/LoginScreen.kt
@@ -1,18 +1,28 @@
 package com.nguyenminhkhang.jarvisai.presentation.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nguyenminhkhang.jarvisai.R
+import com.nguyenminhkhang.jarvisai.presentation.components.CustomButton
 import com.nguyenminhkhang.jarvisai.presentation.components.CustomTextField
 import com.nguyenminhkhang.jarvisai.presentation.components.SignInWithGoogle
 
@@ -26,7 +36,7 @@ data class LoginState(
 @Composable
 fun LoginScreen(modifier: Modifier) {
     Scaffold(
-        containerColor = MaterialTheme.colorScheme.background
+        containerColor = MaterialTheme.colorScheme.surface
     ) {
         Column(
             modifier = modifier
@@ -34,8 +44,21 @@ fun LoginScreen(modifier: Modifier) {
                 .padding(it)
                 .padding(16.dp)
                 .background(MaterialTheme.colorScheme.background),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_ai_star), contentDescription = "App Logo",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.align(Alignment.CenterHorizontally).size(60.dp)
+                )
+                Text("Wellcome", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                Text("Sign in to continue to your AI workspace", fontSize = 13.sp, color = MaterialTheme.colorScheme.onSurfaceVariant)
+            }
+            Spacer(Modifier.height(20.dp))
             CustomTextField(
                 modifier = modifier,
                 text = "",
@@ -47,6 +70,12 @@ fun LoginScreen(modifier: Modifier) {
                 text = "",
                 textFieldTitle = "Password",
                 trailingIcon = painterResource(R.drawable.ic_eye_on)
+            )
+            CustomButton(
+                modifier = modifier,
+                buttonText = "Sign In",
+                trailingIcon = null,
+                onClick = { }
             )
             SignInWithGoogle(
                 modifier = modifier,
